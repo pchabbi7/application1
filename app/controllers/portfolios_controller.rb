e977032bf944d0f7c656728b4c77f8fa156a51d0
pchabbi7 <pri.17feb@gmail.com>
2018-07-13 14:39:53 +0530

    Portfolio : Inder , New , Create method added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,23 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end # index
-end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end # new
+
+  # POST /portfolios
+  # POST /portfolios.json
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle , :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your post is now live.' }
+      else
+        format.html { render :new }
+      end # if condition
+    end # do 
+  end # create
+
+end # class

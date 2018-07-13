6a2e8dcf3642975d8a4108721ae153cf498ef22c
pchabbi7 <pri.17feb@gmail.com>
2018-07-13 15:07:00 +0530

    Portfolios : edit and update method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,7 +5,7 @@ class PortfoliosController < ApplicationController
 	end # index
 
 	def new
-		@portfolio_item = Portfolio.new
+		@portfolio_items = Portfolio.new
 	end # new
 
   # POST /portfolios
@@ -21,5 +21,21 @@ class PortfoliosController < ApplicationController
       end # if condition
     end # do 
   end # create
+
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end # edit
+
+  def update
+    respond_to do |format|
+    	@portfolio_item = Portfolio.find(params[:id])
+
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle , :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end  
 
 end # class

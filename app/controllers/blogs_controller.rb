0c444f785e3f63e356ea47f09bbb863c973489d5
pchabbi7 <pri.17feb@gmail.com>
2018-07-17 12:43:29 +0530

    Added Toggle status in roots and UI

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -61,6 +61,20 @@ class BlogsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+
+  def toggle_status
+
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end # if  
+          
+
+    redirect_to blogs_url, notice: 'Status Updated Successfully'
+
+  end # toggle_status
 
   private
     # Use callbacks to share common setup or constraints between actions.

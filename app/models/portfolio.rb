e1b23844be8730801ff7377202c2988f8d371049
pchabbi7 <pri.17feb@gmail.com>
2018-07-25 23:32:23 +0530

    Implemented after_initialize in portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,11 @@ class Portfolio < ApplicationRecord
 	end 
 
 	scope :ruby_on_rails_portfolio, -> {where(subtitle: 'Ruby on Rails ')}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://via.placeholder.com/600X400"
+		self.thumb_image ||= "http://via.placeholder.com/350X200"
+	end #set_defaults
 end

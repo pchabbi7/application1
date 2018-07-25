5dbd2bdb42bad3e4588a691c968671f78589dcbd
pchabbi7 <pri.17feb@gmail.com>
2018-07-25 23:56:41 +0530

    Implimented Concerns with Portfolio and Skills

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	#Validation of the required feilds
 	validates_presence_of :title, :body, :main_image , :thumb_image
 
@@ -11,7 +12,7 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "http://via.placeholder.com/600X400"
-		self.thumb_image ||= "http://via.placeholder.com/350X200"
+		self.main_image ||= Placeholder.image_generator(600,400)
+		self.thumb_image ||= Placeholder.image_generator(300,200)
 	end #set_defaults
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,12 @@
 class Skill < ApplicationRecord
+	include Placeholder
 	#Validation of the required feilds
 	validates_presence_of :title, :percent_utilized
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.badge ||= Placeholder.image_generator(600,400)
+	end #set_defaults
+
 end
